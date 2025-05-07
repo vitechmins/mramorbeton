dd3f77eac3ce812d7344e6fd3ee31ced56a93f29
Виктор <Irzunovv@gmail.com>
2025-05-07 20:44:09 +0300

    push

--- http/telegramAPI.js
+++ http/telegramAPI.js
@@ -1,7 +1,5 @@
 import axios from 'axios'
 
-// const token = '5562126487:AAGqX2TBd3toX15OgSCQ2yW55RNfgtBWQko'
-// const chat_id = '-1001794221917'
 const token = '7485938744:AAGVBInU8gSyn7w0OGlelHzWvSzNC4C0N1o'
 const chat_id = '-1002186363353'
 const uri_api = `https://api.telegram.org/bot${token}/sendMessage`
